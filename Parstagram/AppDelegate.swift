ba5722e576f258e0a642eaca058dfe3ee110c7ca
Yordanos SoIomon <ybsolomon@dons.usfca.edu>
2021-03-11 21:56:19 -0800

    added assets and onSignUp()

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "YOUR_APP_ID" // <- UPDATE
+                    $0.clientKey = "YOUR_CLIENT_KEY" // <- UPDATE
+                    $0.server = "https://parseapi.back4app.com"
+            }
+        Parse.initialize(with: parseConfig)
         return true
     }
 
